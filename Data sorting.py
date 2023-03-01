00081191dff955774c048dfaad36529c90c09714
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-01 12:08:15 -0500

    Update

--- Data sorting.py
+++ Data sorting.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
 import spacy
-import cleanlab
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 # Load the spacy model
 nlp = spacy.load('en_core_web_sm')
@@ -14,39 +15,29 @@ def extract_data(text, categories=None):
     # Parse the text using spacy
     doc = nlp(text)
 
-    # If categories were not specified, determine them automatically using cleanlab
+    # If categories were not specified, determine them automatically using Scikit-learn
     if categories is None:
-        # Use cleanlab to get the best categories based on the predicted labels
-        true_labels = [ent.label_ for ent in doc.ents]
-        confident_joint = cleanlab.latent_estimation.estimate_joint(
-            true_labels=true_labels, 
-            predicted_labels=true_labels, 
-            psx=None, 
-            confident_joint=None, 
-            calibrate=False, 
-            verbose=False
-        )
-        categories = cleanlab.latent_estimation.estimate_latent(
-            confident_joint=confident_joint, 
-            n_classes=len(set(true_labels))
-        )
+        # Extract the entity text and label
+        X = [ent.text.strip() for ent in doc.ents]
+
+        # Use Scikit-learn to train a Naive Bayes classifier to predict the category
+        vectorizer = CountVectorizer()
+        X_train = vectorizer.fit_transform(X)
+        y_train = [ent.label_ for ent in doc.ents]
+        clf = MultinomialNB()
+        clf.fit(X_train, y_train)
+
+        # Use the trained classifier to predict the category of each entity
+        categories = clf.predict(X_train)
 
     # Loop through the entities in the document
-    for ent in doc.ents:
+    for i, ent in enumerate(doc.ents):
         # Extract the entity text and label
         text = ent.text.strip()
         label = ent.label_
 
-        # Determine the appropriate category for the entity
-        for category in categories:
-            if category.lower() in label.lower():
-                category_label = category
-                break
-        else:
-            # If the label doesn't match any category, skip this entity
-            continue
-
         # Add the item to the appropriate category
+        category_label = categories[i]
         if category_label not in data:
             data[category_label] = []
         data[category_label].append(text)
