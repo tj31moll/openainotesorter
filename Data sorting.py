1bb41962f3e989a6acedd399e1ff8c1ad9c5b77e
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-01 13:03:19 -0500

    Update Data sorting.py

--- Data sorting.py
+++ Data sorting.py
@@ -3,8 +3,9 @@ from tkinter import filedialog
 import spacy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import onenote
 
-# Load the spacy model
+# Load the spaCy model
 nlp = spacy.load('en_core_web_sm')
 
 # Define a function to extract and organize the data
@@ -12,7 +13,7 @@ def extract_data(text, categories=None):
     # Initialize an empty dictionary to store the data
     data = {}
 
-    # Parse the text using spacy
+    # Parse the text using spaCy
     doc = nlp(text)
 
     # If categories were not specified, determine them automatically using Scikit-learn
@@ -45,7 +46,6 @@ def extract_data(text, categories=None):
     # Return the organized data
     return data
 
-
 # Define a function to handle UI input
 def handle_ui_input():
     # Create a Tkinter file dialog to choose a file to open
@@ -58,10 +58,20 @@ def handle_ui_input():
         with open(file_path, 'r') as f:
             text = f.read()
 
-        # Extract the data and print it to the console
+        # Extract the data
         data = extract_data(text)
-        print(data)
 
+        # Add the data to OneNote
+        notebook_name = "My Notebook"
+        section_name = "My Section"
+        page_name = "My Page"
+        on = onenote.OneNote()
+        on.navigate_to(notebook_name=notebook_name, section_name=section_name, page_name=page_name)
+
+        for category in data:
+            on.create_page(notebook_name=notebook_name, section_name=section_name, page_name=category)
+            for item in data[category]:
+                on.add_page_content(notebook_name=notebook_name, section_name=section_name, page_name=category, content=item)
 
 # Call the UI input function
 handle_ui_input()
