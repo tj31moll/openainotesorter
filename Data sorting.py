09cdb3ed6b8bf6a96e478d359dd11c574b5dc821
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-05 10:42:55 -0500

    Update Data sorting.py

--- Data sorting.py
+++ Data sorting.py
@@ -4,20 +4,21 @@ import onenote
 import spacy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from flask import Flask, render_template, request
 
 # Load the spacy model
 nlp = spacy.load('en_core_web_sm')
 
 # Define a function to extract and organize the data
-def extract_data(text, clf=None):
+def extract_data(text, categories=None):
     # Initialize an empty dictionary to store the data
     data = {}
 
     # Parse the text using spacy
     doc = nlp(text)
 
-    # If the classifier is not provided, train a new one
-    if clf is None:
+    # If categories were not specified, determine them automatically using Scikit-learn
+    if categories is None:
         # Extract the entity text and label
         X = [ent.text.strip() for ent in doc.ents]
 
@@ -28,8 +29,8 @@ def extract_data(text, clf=None):
         clf = MultinomialNB()
         clf.fit(X_train, y_train)
 
-    # Use the trained classifier to predict the category of each entity
-    categories = clf.predict(X_train)
+        # Use the trained classifier to predict the category of each entity
+        categories = clf.predict(X_train)
 
     # Loop through the entities in the document
     for i, ent in enumerate(doc.ents):
@@ -43,10 +44,10 @@ def extract_data(text, clf=None):
             data[category_label] = []
         data[category_label].append(text)
 
-    # Return the organized data and the trained classifier
-    return data, clf
+    # Return the organized data
+    return data
 
-# Define a function to create a OneNote notebook and section
+# # Define a function to create a OneNote notebook and section
 def create_notebook(api_key, notebook_name, category_labels):
     # Create a OneNote client using the API key
     client = onenote.OneNoteClient(api_key)
@@ -66,36 +67,40 @@ def create_notebook(api_key, notebook_name, category_labels):
     # Return the notebook and category IDs
     return notebook['id'], category_ids
 
-# Define a function to handle UI input
-def handle_ui_input(api_key):
-    # Create a Tkinter file dialog to choose a file to open
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename()
+# Initialize the Flask application
+# Initialize the Flask application
+app = Flask(__name__)
 
-    # If a file was selected, read the file and extract the data
-    if file_path:
-        with open(file_path, 'r') as f:
-            text = f.read()
+# Define the home page route
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-        # Extract the data and print it to the console
-        data, clf = extract_data(text)
+# Define the upload page route
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the uploaded file
+        file = request.files['file']
+
+        # Read the file and extract the data
+        text = file.read().decode('utf-8')
+        data = extract_data(text)
 
         # Create a new OneNote page for each category and append the items to the page
-        notebook_id, section_id = create_notebook(api_key, notebook_name, list(set(clf.classes_)))
+        api_key = 'YOUR_API_KEY'
+        notebook_name = 'Entity Recognition Data'
+        notebook_id, section_id = create_notebook(api_key, notebook_name, data.keys())
         for category, items in data.items():
             page_title = category
             page_content = '<ul>' + ''.join([f'<li>{item}</li>' for item in items]) + '</ul>'
-            onenote.create_page(api_key, page_title, page_content, section_id)
+            onenote.create_page(api_key, page_title, page_content, section_id[category])
 
-        print('Data successfully added to OneNote!')
+        return 'Data uploaded successfully!'
 
-# Set the OneNote API key and notebook name
+    # If the request method is GET, show the upload form
+    return render_template('upload.html')
 
+if __name__ == '__main__':
+    app.run()
 
-# Set the OneNote API key and notebook name
-api_key = 'YOUR_API_KEY'
-notebook_name = 'Entity Recognition Data'
-
-# Call the UI input function
-handle_ui_input(api_key)
